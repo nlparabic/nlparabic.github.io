52d85bb0535960256a39687f6f055d6195b49560
abdullah-alnahas <abdullah.nahass@gmail.com>
2020-06-28 08:02:37 +0300

    Updates

---
+++ precache-manifest.bec857f05f8f19c66b30cd05aede3747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814c87423b603e8f89ea393eea3d1c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5198aaed7e2e1a0a3ec",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5afa29951b140f472dd7",
+    "url": "/static/js/2.6bb89fc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bb89fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5198aaed7e2e1a0a3ec",
+    "url": "/static/js/main.8fcd2fac.chunk.js"
+  },
+  {
+    "revision": "318322d2df8116b8ffd6",
+    "url": "/static/js/runtime-main.e05fdffa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
